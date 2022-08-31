f691192af34b66856b8857aaac1226ad076140ab
denisdelaki <denisdelaki@gmail.com>
2022-08-31 11:12:26 +0300

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram 
+    attr_accessor :word
+def initialize (word)
+@word=word
+end
+def match (array)
+    anagram=[]
+array.each do |item|
+    if @word.chars.sort==item.chars.sort
+        anagram<<item 
+    end
+end
+    anagram
+end 
+end
